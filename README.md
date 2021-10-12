580fa36703cc89ec782d26acf160cccaac2c41e6
Dennis Piehl <piehld@gmail.com>
2021-10-12 15:49:07 -0400

    V0.10 Created module

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# py-rcsb_utils_dssp
-Utility for calculating DSSP secondary structure entries for models in the RCSB PDB archive.
+# RCSB Python DSSP Utilities
+
+[![Build Status](https://dev.azure.com/rcsb/RCSB%20PDB%20Python%20Projects/_apis/build/status/rcsb.py-rcsb_utils_dssp?branchName=master)](https://dev.azure.com/rcsb/RCSB%20PDB%20Python%20Projects/_build/latest?definitionId=&branchName=master)
+
+## Introduction
+
+This module contains utility methods for calculating DSSP secondary structure entries for models in the RCSB PDB archive.
+
+### Installation
+
+Download the library source software from the project repository:
+
+```bash
+
+git clone --recurse-submodules https://github.com/rcsb/py-rcsb_utils_dssp.git
+
+```
+
+**Important:** Setup will require an up-to-date version of [cmake](https://cmake.org/install/) to be installed on the machine and the executable to be in the system's PATH.
+
+Optionally, run test suite (Python versions 3.9) using
+[setuptools](https://setuptools.readthedocs.io/en/latest/) or
+[tox](http://tox.readthedocs.io/en/latest/example/platform.html):
+
+```bash
+python setup.py test
+
+or simply run
+
+tox
+```
+
+Installation is via the program [pip](https://pypi.python.org/pypi/pip).
+
+```bash
+pip install rcsb.utils.dssp
+
+or for the local repository:
+
+pip install .
+```
+
+## References
+* DSSP 4.0. Maarten L. Hekkelman, et al. https://github.com/PDB-REDO/dssp
+* libcifpp. Maarten L. Hekkelman, et al. https://github.com/PDB-REDO/libcifpp
---
+++ setup.py
@@ -0,0 +1,67 @@
+# File: setup.py
+# Date: 2-Aug-2021
+#
+# Updates:
+#
+#
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+packages = []
+thisPackage = "rcsb.utils.dssp"
+
+with open("rcsb/utils/dssp/__init__.py", "r", encoding="utf-8") as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
+
+# Load packages from requirements*.txt
+with open("requirements.txt", "r", encoding="utf-8") as ifh:
+    packagesRequired = [ln.strip() for ln in ifh.readlines()]
+
+with open("README.md", "r", encoding="utf-8") as ifh:
+    longDescription = ifh.read()
+
+if not version:
+    raise RuntimeError("Cannot find version information")
+
+setup(
+    name=thisPackage,
+    version=version,
+    description="RCSB Python DSSP Utilities",
+    long_description_content_type="text/markdown",
+    long_description=longDescription,
+    author="Dennis Piehl",
+    author_email="dennis.piehl@rcsb.org",
+    url="https://github.com/rcsb/py-rcsb_utils_dssp",
+    #
+    license="Apache 2.0",
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        # 'Development Status :: 5 - Production/Stable',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ),
+    entry_points={"console_scripts": []},
+    #
+    install_requires=packagesRequired,
+    packages=find_packages(exclude=["rcsb.mock-data", "rcsb.utils.tests-dssp", "rcsb.utils.tests-*", "tests.*"]),
+    package_data={
+        # If any package contains *.md or *.rst ...  files, include them:
+        "": ["*.md", "*.rst", "*.txt", "*.cfg"]
+    },
+    #
+    test_suite="rcsb.utils.tests-dssp",
+    tests_require=["tox"],
+    #
+    # Not configured ...
+    extras_require={"dev": ["check-manifest"], "test": ["coverage"]},
+    # Added for
+    command_options={"build_sphinx": {"project": ("setup.py", thisPackage), "version": ("setup.py", version), "release": ("setup.py", version)}},
+    # This setting for namespace package support -
+    zip_safe=False,
+)
